9442712f63bff8482866a61eac8d4ad91a0950aa
opnways <admin@opnways.com>
2022-10-06 09:00:04 +0200

    Refactorize input Search

--- src/home/pages/HomePage.jsx
+++ src/home/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { ProductItem } from '../../productItem/components/ProductItem';
-
+import SearchInput from '../components/SearchInput';
 import { getProducts } from '../helpers/getProducts'
 
 export const HomePage = () => {
@@ -8,15 +8,15 @@ export const HomePage = () => {
 
 const [products, setProducts] = useState([]);
 const [searchTerm, setSearchTerm] = useState("");
-const [searchResult, setSearchResult] = useState([]);
+
 
 //load products 
-let results;
-let productsJSON;
+let results = [];
 const getAllProduct = async () => {
   const newProducts = await getProducts();
   setProducts(newProducts);
 }
+// clear search input
 const clearInput = () => {
   const input = document.querySelector('input');
   input.value = '';
@@ -26,39 +26,29 @@ useEffect(() => {
   getAllProduct();
   clearInput();
   
-}, [productsJSON])
+}, [])
+
 // real time search
-
-
 const handleChange = event => {
   setSearchTerm(event.target.value);
 };
-
-if(typeof products === 'string' ) {
-  // convert to json
-productsJSON = JSON.parse(products);
-} else {
-  // already json
- productsJSON = products;
-}
-
-searchTerm.length > 0 ? results = productsJSON.filter(product => product.brand.toLowerCase().includes(searchTerm.toLowerCase()) ||
-    product.model.toLowerCase().includes(searchTerm.toLowerCase())) : results = productsJSON;
+// copare search term with product name
+searchTerm.length > 0 ? results = products.filter(product => product.brand.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    product.model.toLowerCase().includes(searchTerm.toLowerCase())) : results = products;
 
 return (
   <div className='container'>
     <div className='row'>
       <div className='col-12'>
         <h1>Home</h1>
-       
-        <input
-          type="text"
-          placeholder="Search"
-          value={searchTerm}
-          onChange={handleChange}
-        />
-
-        
+        <SearchInput
+                  type="text"
+                
+                  label="Search a product"
+                  value={searchTerm}
+                  placeholder="By model or by brand"
+                  handleChange={handleChange}
+                />
        
       </div>
     </div>
