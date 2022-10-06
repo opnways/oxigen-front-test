99da4839dc0fd1701780004ca1e7972f4f25048f
opnways <admin@opnways.com>
2022-10-06 10:34:01 +0200

    Clean getPrducts

--- src/home/helpers/getProducts.js
+++ src/home/helpers/getProducts.js
@@ -1,16 +1,28 @@
 export const getProducts = async () => {
-        const url = "https://front-test-api.herokuapp.com/api/product";
-        const resp = await fetch(url);
-        const data = await resp.json();
-      
-        const products = data.map(product =>({
-          id: product.id,
-          img: product.imgUrl,
-          brand: product.brand,
-          model: product.model,
-          price: product.price,
-      
-        }));
-       
-        return products;
+  // check if the products are already in local storage
+  const productsCache = localStorage.getItem("products");
+
+  if (productsCache) {
+    const dateCache = localStorage.getItem("date");
+    if (dateCache > new Date().getTime() - 1000 * 60 * 60) {
+      const products = JSON.parse(productsCache);
+      return products;
+    } else {
+      localStorage.removeItem("products");
+      localStorage.removeItem("date");
     }
+  }
+
+  // if not, fetch them from the server
+  const url = "https://front-test-api.herokuapp.com/api/product";
+  const resp = await fetch(url);
+  const data = await resp.json();
+  const products = data.map((product) => ({
+    ...product,
+    img: product.imgUrl,
+  }));
+  // save them in local storage
+  localStorage.setItem("products", JSON.stringify(products));
+  localStorage.setItem("date", new Date().getTime());
+  return products;
+};
