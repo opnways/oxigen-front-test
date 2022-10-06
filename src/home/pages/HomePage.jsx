0253ca6861206160c92667c829a528dc80d2258a
opnways <admin@opnways.com>
2022-10-06 08:32:44 +0200

    Refactorization & clean input
    Refactorization if  exist search

--- src/home/pages/HomePage.jsx
+++ src/home/pages/HomePage.jsx
@@ -1,61 +1,74 @@
 import React, { useEffect, useState } from 'react'
 import { ProductItem } from '../../productItem/components/ProductItem';
+
 import { getProducts } from '../helpers/getProducts'
 
 export const HomePage = () => {
 
 
 const [products, setProducts] = useState([]);
+const [searchTerm, setSearchTerm] = useState("");
+const [searchResult, setSearchResult] = useState([]);
 
-// real
+//load products 
 let results;
+let productsJSON;
 const getAllProduct = async () => {
   const newProducts = await getProducts();
   setProducts(newProducts);
 }
+const clearInput = () => {
+  const input = document.querySelector('input');
+  input.value = '';
 
+  }
 useEffect(() => {
   getAllProduct();
-}, [])
-
+  clearInput();
+  
+}, [productsJSON])
 // real time search
-const [searchTerm, setSearchTerm] = useState("");
-const [searchResult, setSearchResult] = useState([]);
+
 
 const handleChange = event => {
   setSearchTerm(event.target.value);
 };
 
-
-
-useEffect(() => {
-  const results = products.filter(product =>
-    product.brand.toLowerCase().includes(searchTerm.toLowerCase()) ||
-    product.model.toLowerCase().includes(searchTerm.toLowerCase()) 
-  );
-  setSearchResult(results);
-}, [searchTerm]);
-
-if(searchResult === undefined || searchResult.length == 0){
-results = products;
+if(typeof products === 'string' ) {
+  // convert to json
+productsJSON = JSON.parse(products);
 } else {
-results = searchResult;
+  // already json
+ productsJSON = products;
 }
+
+searchTerm.length > 0 ? results = productsJSON.filter(product => product.brand.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    product.model.toLowerCase().includes(searchTerm.toLowerCase())) : results = productsJSON;
 
 return (
   <div className='container'>
     <div className='row'>
       <div className='col-12'>
         <h1>Home</h1>
+       
         <input
           type="text"
           placeholder="Search"
           value={searchTerm}
           onChange={handleChange}
         />
+
+        
+       
       </div>
     </div>
     <div className='row'>
+
+
+
+
+
+
       {results.map(product => (
         <ProductItem key={product.id} {...product} />
       ))}
@@ -63,7 +76,3 @@ return (
   </div>
 )
 }
-
-
-
-
